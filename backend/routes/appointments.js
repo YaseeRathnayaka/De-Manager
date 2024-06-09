ce1cb5a284a1f32fd9660c48f17741a0358a0b68
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-06-09 19:14:28 +0530

    Created end point for update appointment

--- backend/routes/appointments.js
+++ backend/routes/appointments.js
@@ -24,13 +24,39 @@ router.post('/', auth, async (req, res) => {
     }
 })
 
-router.get('/all', auth, async(req, res) => {
+router.get('/all', auth, async (req, res) => {
     try {
-        const appointments = await Appointment.find({user: req.user._id}).select('-user -_id')
+        const appointments = await Appointment.find({ user: req.user._id }).select('-user -_id')
         res.send(appointments)
     } catch (error) {
         res.status(500).send('Error fetching appointments.')
     }
+})
+
+router.put('/:id', auth, async (req, res) => {
+    const { error } = validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    try {
+        const appointment = await Appointment.findByIdAndUpdate(
+            req.params.id,
+            {
+                user: req.user._id,
+                vehicle: req.body.vehicle,
+                serviceType: req.body.serviceType,
+                appointmentDate: req.body.appointmentDate,
+                appointmentTime: req.body.appointmentTime,
+                status: req.body.status
+            },
+            { new: true }
+        )
+
+        if (!appointment) return res.status(404).send('The appointment with the given ID was not found.')
+        res.send(appointment)
+    } catch (error) {
+        res.status(500).send('An error occurred while updating the appointment.');
+    }
+
 })
 
 module.exports = router
