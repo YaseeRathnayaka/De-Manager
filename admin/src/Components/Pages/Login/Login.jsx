974f74389ea033146679948e364f15f63432fa9e
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-06-26 14:52:32 +0530

    created basic UI in login

--- admin/src/Components/Pages/Login/Login.jsx
+++ admin/src/Components/Pages/Login/Login.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className="flex h-screen">
+      <div className='bg-gray-800 w-2/5 h-full'>
+        <div className='bg-purple-100 w-full h-4/5 mt-20'>
+
+        </div>
+          </div>
+      <div className='w-3/5 h-full '>
+      <div className='bg-gray-300 w-full h-4/5 mt-20 '>
+      </div>
+        </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
