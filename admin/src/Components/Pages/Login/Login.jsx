6d79c6a0c2a18dc7e8fafb271afce15b60a7da73
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-06-26 14:59:35 +0530

    finalized UI

--- admin/src/Components/Pages/Login/Login.jsx
+++ admin/src/Components/Pages/Login/Login.jsx
@@ -4,12 +4,12 @@ const Login = () => {
   return (
     <div className="flex h-screen">
       <div className='bg-gray-800 w-2/5 h-full'>
-        <div className='bg-purple-100 w-full h-4/5 mt-20'>
+        <div className='bg-purple-100 w-full h-4/5 mt-20 ml-20'>
 
         </div>
           </div>
       <div className='w-3/5 h-full '>
-      <div className='bg-gray-300 w-full h-4/5 mt-20 '>
+      <div className='bg-gray-300 w-11/12 h-4/5 mt-20 mr-40'>
       </div>
         </div>
     </div>
