84847da0fe486cf445cc6de8c2087620b01d8120
isurubandara1 <isurumbandara@gmail.com>
2024-06-27 14:03:39 +0530

    first page add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
+import FirstPage from "./Pages/FirstPage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!, ddddd,ffffffff
-    </h1>
+    <FirstPage/>
   )
 }
