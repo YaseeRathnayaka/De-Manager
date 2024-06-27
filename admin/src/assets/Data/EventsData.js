e7d089ed8ce5d394c5c6e0c87e79721104953801
Yasee Rathnayaka <rmybrathnayaka@std.appsc.sab.ac.lk>
2024-06-27 09:06:48 +0530

    Update EventsData.js

--- admin/src/assets/Data/EventsData.js
+++ admin/src/assets/Data/EventsData.js
@@ -22,14 +22,14 @@ const events = [
     {
       appointment_id: 1,
       title: 'Full Service abs3456',
-      start: new Date(2024, 5, 22, 10, 0), // June 22, 2024, 10:00 AM
-      end: new Date(2024, 5, 22, 12, 0), // June 22, 2024, 11:00 AM
+      start: new Date(2024, 5, 24, 10, 0), // June 22, 2024, 10:00 AM
+      end: new Date(2024, 5, 24, 12, 0), // June 22, 2024, 11:00 AM
     },
     {
       appointment_id: 1,
       title: 'Full Service abs3456',
-      start: new Date(2024, 5, 22, 10, 0), // June 22, 2024, 10:00 AM
-      end: new Date(2024, 5, 22, 12, 0), // June 22, 2024, 11:00 AM
+      start: new Date(2024, 5, 24, 10, 0), // June 22, 2024, 10:00 AM
+      end: new Date(2024, 5, 24, 12, 0), // June 22, 2024, 11:00 AM
     },
     {
       appointment_id: 1,
