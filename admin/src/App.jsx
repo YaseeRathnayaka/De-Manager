a38f22131ada1f6260d4417b983726c814e7f0e6
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-06-27 09:21:24 +0530

    recorrected

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Pages/Home/Home';
-import Schedule from './Components/Pages/Schedule/Schedule'
-import CalenderView from './Components/Pages/CalenderView/CalenderView'
+import Schedule from './Components/Pages/Schedule/Schedule';
+import CalendarView from './Components/Pages/CalenderView/CalenderView';
 import ListView from './Components/Pages/ListView/ListView';
+import { AppointmentProvider } from './contexts/AppointmentContext'; // Adjust this import based on your project structure
 import Login from './Components/Pages/Login/Login';
-
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/calendar" element={<CalenderView />} />
-          <Route path="/listview" element={<ListView />} />
-          <Route path="login" element={<Login />}  />
-        </Routes>
-      </div>
+      <AppointmentProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/schedule-appointment" element={<Schedule />} />
+            <Route path="/calendar" element={<CalendarView />} />
+            <Route path="/listview" element={<ListView />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </AppointmentProvider>
     </Router>
   );
 }
---
+++ admin/src/contexts/AppointmentContext.jsx
@@ -0,0 +1,18 @@
+// src/contexts/AppointmentContext.js
+import React, { createContext, useState } from 'react';
+
+export const AppointmentContext = createContext();
+
+export const AppointmentProvider = ({ children }) => {
+  const [appointments, setAppointments] = useState([]);
+
+  const addAppointment = (appointment) => {
+    setAppointments([...appointments, appointment]);
+  };
+
+  return (
+    <AppointmentContext.Provider value={{ appointments, addAppointment }}>
+      {children}
+    </AppointmentContext.Provider>
+);
+};
