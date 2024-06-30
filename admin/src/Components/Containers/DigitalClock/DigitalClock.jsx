7fc9655db999cc04ddabc6031cb3d213eae048aa
Yasee Rathnayaka <rmybrathnayaka@std.appsc.sab.ac.lk>
2024-06-30 23:13:33 +0530

    feedback

--- admin/src/Components/Containers/DigitalClock/DigitalClock.jsx
+++ admin/src/Components/Containers/DigitalClock/DigitalClock.jsx
@@ -22,7 +22,7 @@ const DigitalClock = () => {
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    return `${day}-${month}-${year}`;
   };
 
   return (
--- admin/src/Components/Containers/Feedback/Feedback.jsx
+++ admin/src/Components/Containers/Feedback/Feedback.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import feedbackData from '../../../assets/Data/feedbackData';
+
+// Register the components
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend
+);
 
 const Feedback = ({ className }) => {
+  const data = {
+    labels: feedbackData.map(item => item.label),
+    datasets: [
+      {
+        label: 'Feedback',
+        data: feedbackData.map(item => item.count),
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+        ],
+        borderColor: [
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(255, 99, 132, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+   
+  };
+
   return (
     <div className={`mx-1 bg-custom-light-blue rounded-xl p-4 ${className} w-1/5`}>
-     Feedback
-     
+      <h2>Feedback</h2>
+      <Doughnut data={data} className='mt-11' />
     </div>
   );
-}
+};
 
 export default Feedback;
