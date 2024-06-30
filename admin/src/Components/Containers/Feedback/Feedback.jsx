ccc09613616dee97b0e050029285cb3c5b5ec205
Yasee Rathnayaka <rmybrathnayaka@std.appsc.sab.ac.lk>
2024-06-30 23:18:08 +0530

    Update Feedback.jsx

--- admin/src/Components/Containers/Feedback/Feedback.jsx
+++ admin/src/Components/Containers/Feedback/Feedback.jsx
@@ -23,25 +23,42 @@ const Feedback = ({ className }) => {
         label: 'Feedback',
         data: feedbackData.map(item => item.count),
         backgroundColor: [
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.6)',  // Brighter blue
+          'rgba(255, 206, 86, 0.6)',  // Brighter yellow
+          'rgba(255, 99, 132, 0.6)',  // Brighter red
         ],
         borderColor: [
-          'rgba(75, 192, 192, 1)',
+          'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(255, 99, 132, 1)',
         ],
         borderWidth: 1,
       },
     ],
-   
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom', // Position the legend below the chart
+        labels: {
+          font: {
+            size: 14, // Adjust the font size if necessary
+            color: '#000', // Adjust the font color if necessary
+          },
+        },
+      },
+    },
+    animation: {
+      duration: 1500, // Animation duration
+    },
   };
 
   return (
-    <div className={`mx-1 bg-custom-light-blue rounded-xl p-4 ${className} w-1/5`}>
-      <h2>Feedback</h2>
-      <Doughnut data={data} className='mt-11' />
+    <div className={`mx-1 bg-custom-light-blue rounded-xl p-4 ${className} w-1/5 `}>
+      <h2 className='mb-5'>Feedback</h2>
+      <Doughnut data={data} options={options} />
     </div>
   );
 };
