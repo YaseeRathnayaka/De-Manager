4428896c8266397ddf165e1be94c9b982bee8b57
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-07-08 14:32:09 +0530

    Update Login.jsx

--- admin/src/Components/Pages/Login/Login.jsx
+++ admin/src/Components/Pages/Login/Login.jsx
@@ -21,13 +21,13 @@ const Login = () => {
 
   return (
     <div className="flex h-screen">
-       <div className='bg-gray-800 w-2/5 h-full'>
+       <div className='bg-gray-800 w-3/5 h-full'>
         <div className='bg-purple-100 w-full h-4/5 mt-20 ml-20'>
-        <img src={profile} alt="Profile" className="max-w-full h-72  ml-20 pt-20" />
+        <img src={profile} alt="Profile" className="w-auto h-full " />
           </div>
       </div>
 
-      <div className='w-3/5 h-full '>
+      <div className='w-2/5 h-full '>
       <div className='bg-gray-300 w-11/12 h-4/5 mt-20 mr-40'>
       <div className="w-3/5 flex flex-col justify-center items-center p-32 ml-48">
                 <h2 className="text-2xl font-bold mb-4">Login Your Account</h2>
