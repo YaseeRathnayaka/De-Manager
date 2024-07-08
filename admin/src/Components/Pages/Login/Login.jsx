f632f971ada6794bc040408d1cdb22bc7d7bbc40
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-07-08 11:08:01 +0530

    login part

--- admin/src/Components/Pages/Login/Login.jsx
+++ admin/src/Components/Pages/Login/Login.jsx
@@ -1,51 +1,73 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+import profile from './../../../assets/car.png';
+
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
-    // Perform login logic here, e.g., call an authentication function
-    // For example, you might use Firebase authentication:
-    // auth.signInWithEmailAndPassword(email, password).then(...)
+  const handleLogin = () =>{
+    console.log('Email:' ,email);
+    console.log('Password:' , password);
   };
 
-  const handleEmailChange = (e) => {
+  const handleEmailChange = (e) =>{
     setEmail(e.target.value);
-    
   };
-
-  const handlePasswordChange = (e) => {
+  
+  const handlePasswordChange = (e) =>{
     setPassword(e.target.value);
   };
 
   return (
-    <div className='container p-4 rounded-md bg-slate-600'>
-      <div className="flex flex-col p-4 mb-4 rounded-md bg-slate-300">
-        <label>Email:</label>
-        <input
-          type="text"
-          placeholder='email'
-          value={email}
-          onChange={handleEmailChange}
-          className="p-2 rounded-md"
-        />
+    <div className="flex h-screen">
+       <div className='bg-gray-800 w-2/5 h-full'>
+        <div className='bg-purple-100 w-full h-4/5 mt-20 ml-20'>
+        <img src={profile} alt="Profile" className="max-w-full h-72  ml-20 pt-20" />
+          </div>
       </div>
-      <div className="flex flex-col p-4 mb-4 rounded-md bg-slate-300">
-        <label>Password:</label>
-        <input
-          type="password"
-          placeholder='password'
-          value={password}
-          onChange={handlePasswordChange}
-          className="p-2 rounded-md"
-        />
+
+      <div className='w-3/5 h-full '>
+      <div className='bg-gray-300 w-11/12 h-4/5 mt-20 mr-40'>
+      <div className="w-3/5 flex flex-col justify-center items-center p-32 ml-48">
+                <h2 className="text-2xl font-bold mb-4">Login Your Account</h2>
+
+            <div className='mb-5'>
+              <input
+               type='text'
+               placeholder='Email'
+               value={email}
+               onChange={handleEmailChange}
+               className='w-96 p-3 rounded-md border bg-purple-100 focus:outline-none focus:border-gray-800'/>
+            </div>    
+
+           <div className='mb-5'>
+              <input
+               type='text'
+               placeholder='Password'
+               value={email}
+               onChange={handlePasswordChange}
+               className='p-3 rounded-md border bg-purple-100 focus:outline-none focus:border-gray-800 w-96'/>
+           </div>  
+
+            <button onClick={handleLogin} className='w-96 p-3 bg-purple-800 text-white rounded-md mb-4 hover:bg-purple-900'>
+             Login
+             </button>
+
+             <div className='text-center'>
+             <a href=" " className="text-purple-700 hover:underline">Forgot Password</a>
+             </div>
+
+             
+       </div>
+       
       </div>
-      <button onClick={handleLogin} className="p-2 text-white bg-blue-500 rounded-md">Submit</button>
-    </div>
+      
+      </div>
+      
+      </div>
+      
   );
-};
+}
 
 export default Login;
