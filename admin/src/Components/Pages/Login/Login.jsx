e379fcf213702b3e707c9833cbeb95c956a93cc0
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-07-08 14:30:34 +0530

    Update Login.jsx

--- admin/src/Components/Pages/Login/Login.jsx
+++ admin/src/Components/Pages/Login/Login.jsx
@@ -1,68 +1,73 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import profile from './../../../assets/Login2.jpg';
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
+const Login = () => {
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
+
+  const handleLogin = () =>{
+    console.log('Email:' ,email);
+    console.log('Password:' , password);
   };
 
-  const handleEmailChange = (e) => {
+  const handleEmailChange = (e) =>{
     setEmail(e.target.value);
   };
-
-  const handlePasswordChange = (e) => {
+  
+  const handlePasswordChange = (e) =>{
     setPassword(e.target.value);
   };
 
   return (
-    <div className="relative flex h-screen">
-      <div className='absolute top-0 left-0 z-0 w-3/5 h-full bg-gray-800'></div>
-
-      <div className='relative z-10 flex w-2/5 h-full bg-purple-100'>
-        <img src={profile} alt="Profile" className="max-w-full m-auto h-72" />
+    <div className="flex h-screen">
+       <div className='bg-gray-800 w-2/5 h-full'>
+        <div className='bg-purple-100 w-full h-4/5 mt-20 ml-20'>
+        <img src={profile} alt="Profile" className="max-w-full h-72  ml-20 pt-20" />
+          </div>
       </div>
 
-      <div className='relative z-10 flex items-center justify-center w-3/5 h-full'>
-        <div className='w-11/12 p-8 mt-20 mr-40 bg-gray-300 rounded-lg'>
-          <div className="flex flex-col items-center justify-center">
-            <h2 className="mb-4 text-2xl font-bold">Login Your Account</h2>
+      <div className='w-3/5 h-full '>
+      <div className='bg-gray-300 w-11/12 h-4/5 mt-20 mr-40'>
+      <div className="w-3/5 flex flex-col justify-center items-center p-32 ml-48">
+                <h2 className="text-2xl font-bold mb-4">Login Your Account</h2>
 
             <div className='mb-5'>
               <input
-                type='text'
-                placeholder='Email'
-                value={email}
-                onChange={handleEmailChange}
-                className='p-3 bg-purple-100 border rounded-md w-96 focus:outline-none focus:border-gray-800'
-              />
+               type='text'
+               placeholder='Email'
+               value={email}
+               onChange={handleEmailChange}
+               className='w-96 p-3 rounded-md border bg-purple-100 focus:outline-none focus:border-gray-800'/>
             </div>    
 
-            <div className='mb-5'>
+           <div className='mb-5'>
               <input
-                type='password'
-                placeholder='Password'
-                value={password}
-                onChange={handlePasswordChange}
-                className='p-3 bg-purple-100 border rounded-md focus:outline-none focus:border-gray-800 w-96'
-              />
-            </div>  
+               type='text'
+               placeholder='Password'
+               value={email}
+               onChange={handlePasswordChange}
+               className='p-3 rounded-md border bg-purple-100 focus:outline-none focus:border-gray-800 w-96'/>
+           </div>  
 
-            <button onClick={handleLogin} className='p-3 mb-4 text-white bg-purple-800 rounded-md w-96 hover:bg-purple-900'>
-              Login
-            </button>
+            <button onClick={handleLogin} className='w-96 p-3 bg-purple-800 text-white rounded-md mb-4 hover:bg-purple-900'>
+             Login
+             </button>
 
-            <div className='text-center'>
-              <a href=" " className="text-purple-700 hover:underline">Forgot Password</a>
-            </div>
-          </div>
-        </div>
+             <div className='text-center'>
+             <a href=" " className="text-purple-700 hover:underline">Forgot Password</a>
+             </div>
+
+             
+       </div>
+       
       </div>
-    </div>
+      
+      </div>
+      
+      </div>
+      
   );
-};
+}
 
 export default Login;
